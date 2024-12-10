392e1d90e5e6913a8e4f582b3a87f87e1cb21f2f
jkky-98 <aal2525@ajou.ac.kr>
2024-12-10 09:54:06 +0900

    feat: LikeRepository, LikeRepositoryTest

---
+++ noteJ/src/main/java/com/github/jkky_98/noteJ/repository/LikeRepository.java
@@ -0,0 +1,7 @@
+package com.github.jkky_98.noteJ.repository;
+
+import com.github.jkky_98.noteJ.domain.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
