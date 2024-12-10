32d812930d06b620a1fe99dea25da7182f2e25e9
jkky-98 <aal2525@ajou.ac.kr>
2024-12-10 11:55:57 +0900

    feat: FileStoreTest

--- noteJ/src/test/java/com/github/jkky_98/noteJ/file/FileStoreTest.java
+++ noteJ/src/test/java/com/github/jkky_98/noteJ/file/FileStoreTest.java
@@ -1,14 +1,12 @@
 package com.github.jkky_98.noteJ.file;
 
 import com.github.jkky_98.noteJ.domain.FileMetadata;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.context.TestPropertySource;
 
 import java.io.File;
 import java.io.IOException;
