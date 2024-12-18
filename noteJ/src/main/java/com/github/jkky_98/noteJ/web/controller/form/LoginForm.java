2796859a61247196dfa927f15d517e3c2bd037df
jkky-98 <aal2525@ajou.ac.kr>
2024-12-18 09:43:13 +0900

    chore: LoginForm 검증 NotEmpty -> NotBlank

--- noteJ/src/main/java/com/github/jkky_98/noteJ/web/controller/form/LoginForm.java
+++ noteJ/src/main/java/com/github/jkky_98/noteJ/web/controller/form/LoginForm.java
@@ -1,5 +1,6 @@
 package com.github.jkky_98.noteJ.web.controller.form;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -7,10 +8,10 @@ import lombok.Data;
 @Data
 public class LoginForm {
 
-    @NotEmpty
+    @NotBlank(message = "사용자 이름을 입력해주세요.")
     private String username;
 
-    @NotEmpty
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
 
