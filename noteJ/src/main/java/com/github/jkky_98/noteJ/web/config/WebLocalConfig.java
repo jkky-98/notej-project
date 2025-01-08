6efc44e4b461b8e98dcafba5ab2e81a3fba8860e
홍준기 <piehealthcare@hongjungiui-Macmini.local>
2025-01-08 17:16:58 +0900

    chore: fileDir 환경변수화, WebLocalConfig 설정변수 따르기

--- noteJ/src/main/java/com/github/jkky_98/noteJ/web/config/WebLocalConfig.java
+++ noteJ/src/main/java/com/github/jkky_98/noteJ/web/config/WebLocalConfig.java
@@ -1,5 +1,6 @@
 package com.github.jkky_98.noteJ.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @Profile("local")
 public class WebLocalConfig implements WebMvcConfigurer {
+    @Value("${file.dir}")
+    private String fileDir;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 외부 디렉토리를 정적 리소스로 서빙하도록 설정
         registry.addResourceHandler("/storage/**")  // /storage/ 경로로 접근
-                .addResourceLocations("file:/Users/jkky/Documents/private/noteJ-storage/");
+                .addResourceLocations("file:" + fileDir);
     }
 }
