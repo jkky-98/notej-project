80158b6ae66517f8c3f3ea86da1e302607d3a880
jkky-98 <aal2525@ajou.ac.kr>
2025-01-12 17:54:36 +0900

    refactor: comment기능 분리

--- noteJ/src/main/java/com/github/jkky_98/noteJ/web/controller/CommentController.java
+++ noteJ/src/main/java/com/github/jkky_98/noteJ/web/controller/CommentController.java
@@ -19,34 +19,34 @@ public class CommentController {
     private final PostService postService;
     private final CommentService commentService;
 
-//    @PostMapping("/@{username}/post/{postUrl}")
-//    public String writeComment(
-//            @PathVariable("username") String username, // 포스트 주인의 username
-//            @PathVariable("postUrl") String postUrl, // 포스트의 url
-//            @SessionAttribute("loginUser") User sessionUser, // 코멘트 입력자
-//            @Validated @ModelAttribute("commentForm") CommentForm commentForm, // 코멘트 폼 데이터
-//            @RequestHeader("Referer") String referer,
-//            BindingResult bindingResult,
-//            Model model
-//    ) {
-//
-//        if (bindingResult.hasErrors()) {
-//            System.out.println("검증 오류 발생:");
-//            PostViewDto postViewDto = postService.getPost(username, postUrl);
-//
-//            model.addAttribute("postViewDto", postViewDto);
-//            return "postView";
-//        }
-//
-//        // 쿼리 파라미터 제거 로직
-//        if (referer != null) {
-//            int queryIndex = referer.indexOf("?");
-//            if (queryIndex != -1) {
-//                referer = referer.substring(0, queryIndex); // 쿼리 파라미터 제거
-//            }
-//        }
-//
-//        commentService.saveComment(commentForm, sessionUser, postUrl, username);
-//        return "redirect:" + (referer != null ? referer : "/");
-//    }
+    @PostMapping("/@{username}/post/{postUrl}")
+    public String writeComment(
+            @PathVariable("username") String username, // 포스트 주인의 username
+            @PathVariable("postUrl") String postUrl, // 포스트의 url
+            @SessionAttribute("loginUser") User sessionUser, // 코멘트 입력자
+            @Validated @ModelAttribute("commentForm") CommentForm commentForm, // 코멘트 폼 데이터
+            BindingResult bindingResult,
+            @RequestHeader("Referer") String referer,
+            Model model
+    ) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("검증 오류 발생:");
+            PostViewDto postViewDto = postService.getPost(username, postUrl);
+
+            model.addAttribute("postViewDto", postViewDto);
+            return "postView";
+        }
+
+        // 쿼리 파라미터 제거 로직
+        if (referer != null) {
+            int queryIndex = referer.indexOf("?");
+            if (queryIndex != -1) {
+                referer = referer.substring(0, queryIndex); // 쿼리 파라미터 제거
+            }
+        }
+
+        commentService.saveComment(commentForm, sessionUser, postUrl, username);
+        return "redirect:" + (referer != null ? referer : "/");
+    }
 }
