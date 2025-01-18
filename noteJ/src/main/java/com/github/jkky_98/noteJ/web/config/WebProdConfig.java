8746d4e60614a7118e06630eba76617957c960e5
jkky-98 <aal2525@ajou.ac.kr>
2025-01-18 15:46:35 +0900

    fix: 사진 다운로드 실패중

--- noteJ/src/main/java/com/github/jkky_98/noteJ/web/config/WebProdConfig.java
+++ noteJ/src/main/java/com/github/jkky_98/noteJ/web/config/WebProdConfig.java
@@ -1,7 +1,5 @@
 package com.github.jkky_98.noteJ.web.config;
 
-import com.amazonaws.services.s3.AmazonS3;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,20 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile("!local")
 public class WebProdConfig implements WebMvcConfigurer {
 
-    private final AmazonS3 amazonS3;
-    private final String s3BucketName;
-
-    @Autowired
-    public WebProdConfig(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String s3BucketName) {
-        this.amazonS3 = amazonS3;
-        this.s3BucketName = s3BucketName;
-    }
+    @Value("${cloud.aws.s3.bucket}")
+    private String s3BucketName;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // /storage/** 경로를 통해 S3 파일에 접근하도록 설정
         registry.addResourceHandler("/storage/**")
                 // S3 버킷을 URL로 사용
-                .addResourceLocations("https://"+s3BucketName+".s3.ap-northeast-2.amazonaws.com/");
+                .addResourceLocations("https://"+s3BucketName+".s3.amazonaws.com/");
     }
+
 }
