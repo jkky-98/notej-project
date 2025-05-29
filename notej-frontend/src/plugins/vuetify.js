9cfea049c3797510f1c452c74c803296b2277828
piehealthcare <aal2525@ajou.ac.kr>
2025-05-29 16:19:31 +0900

    style(theme): 커스텀 색상 기반 Vuetify 다크/라이트 테마 적용

--- notej-frontend/src/plugins/vuetify.js
+++ notej-frontend/src/plugins/vuetify.js
@@ -14,6 +14,26 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'light', // ← 이 부분만 조정해도 됨
+    themes: {
+      light: {
+        colors: {
+          background: '#bdd4e7',
+          surface: '#aab9cf',
+          primary: '#8693ab',
+          secondary: '#637074',
+          text: '#212227',
+        },
+      },
+      dark: {
+        colors: {
+          background: '#212227',
+          surface: '#637074',
+          primary: '#8693ab',
+          secondary: '#aab9cf',
+          text: '#bdd4e7',
+        },
+      },
+    },
   },
 })
