a99d8e60f650586802b64bf93aefbfa87aac9b11
jkky-98 <aal2525@ajou.ac.kr>
2025-06-01 20:15:13 +0900

    feat(api서버) : 스프링부트 프로젝트 생성

---
+++ notej-backend/notej-api/src/test/java/me/notej/notej_api/NotejApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.notej.notej_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotejApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
